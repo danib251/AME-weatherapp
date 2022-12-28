064ce795ed7ad7cd7df6197a37b84ebe20e64214
Daniel Becerra <daniel.becerra@estudiants.urv.cat>
2022-12-28 19:48:47 +0100

    fase bluetooth ok

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.myapplication;
 
-import android.content.res.Resources;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,29 +34,224 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.DecimalFormat;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText location;
     TextView textResult;
+    TextView textResult2;
     ImageView imageView;
     private final String url = "https://api.openweathermap.org/data/2.5/weather";
     private final String id = "f442a168deb9982284e09f15fd71071b";
     DecimalFormat df = new DecimalFormat("#.##");
+    Intent enableBtIntent;
+    BluetoothDevice currentDevice;
+    BluetoothManager bluetoothManager;
+    BluetoothAdapter bluetoothAdapter;
+    private static final String TAG = "MY_APP_DEBUG_TAG";
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
 
+                //Success
+
+                byte[] b = (byte[]) msg.obj;
+                String m = new String(b);
+
+                textResult2.setText("");
+                textResult2.setText(m);
+                Log.e("", "Msg is:" + m);
+
+        }
+    };
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        location =findViewById(R.id.etCity);
-        textResult=findViewById(R.id.tvResult);
-        imageView=findViewById(R.id.imageView3);
+        location = findViewById(R.id.etCity);
+        textResult = findViewById(R.id.tvResult);
+        textResult2 = findViewById(R.id.textView2);
+        imageView = findViewById(R.id.imageView3);
+        BluetoothAdapter BA;
+        BA = BluetoothAdapter.getDefaultAdapter();
+        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        //startActivityForResult(turnOn, 0);
+        connectBluetooth();
     }
+
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        @SuppressLint("MissingPermission")
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(device.getUuids()[0].getUuid());
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        @SuppressLint("MissingPermission")
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            //bluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            ConnectedThread connectedThread = new ConnectedThread(mmSocket);
+            connectedThread.start();
+        }
+
+        private class ConnectedThread extends Thread {
+            private final BluetoothSocket mmSocket;
+            private final InputStream mmInStream;
+            private final OutputStream mmOutStream;
+            private byte[] mmBuffer; // mmBuffer store for the stream
+
+            public ConnectedThread(BluetoothSocket socket) {
+                mmSocket = socket;
+                InputStream tmpIn = null;
+                OutputStream tmpOut = null;
+
+                // Get the input and output streams; using temp objects because
+                // member streams are final.
+                try {
+                    tmpIn = socket.getInputStream();
+                } catch (IOException e) {
+                    Log.e(TAG, "Error occurred when creating input stream", e);
+                }
+                try {
+                    tmpOut = socket.getOutputStream();
+                } catch (IOException e) {
+                    Log.e(TAG, "Error occurred when creating output stream", e);
+                }
+
+                mmInStream = tmpIn;
+                mmOutStream = tmpOut;
+            }
+
+            public void run() {
+                mmBuffer = new byte[45];
+                int numBytes; // bytes returned from read()
+
+                // Keep listening to the InputStream until an exception occurs.
+                while (true) {
+                    try {
+                        // Read from the InputStream.
+                        numBytes = mmInStream.read(mmBuffer);
+                        // Send the obtained bytes to the UI activity.
+                        Message readMsg = handler.obtainMessage(
+                                0, numBytes, -1,
+                                mmBuffer);
+                        readMsg.sendToTarget();
+                        Thread.sleep(1000);
+                    } catch (IOException | InterruptedException e) {
+                        Log.d(TAG, "Input stream was disconnected", e);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
+
+
+
+    public void connectBluetooth() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            bluetoothManager = getSystemService(BluetoothManager.class);
+        }
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 0);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                if(deviceHardwareAddress.equalsIgnoreCase("00:0e:ea:cf:5d:de")){
+                    currentDevice=device;
+                    ConnectThread connectThread=new ConnectThread(currentDevice);
+                    connectThread.start();
+                    break;
+                }
+            }
+            if (currentDevice==null)
+                return;
+        }
+
+    }
+
+
+
 
     public void getWeatherDetails(View view) {
         String tempUrl = "";
         String city = location.getText().toString().trim();
+
+
+
+
         if (city.equals("")) {
             textResult.setText("City field can not be empty!");
         } else {
@@ -67,19 +273,21 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
                         String countryName = jsonObjectSys.getString("country");
                         String cityName = jsonResponse.getString("name");
+                        String id = jsonResponse.getString("id");
                         textResult.setTextColor(Color.rgb(68,134,199));
 
                         output += "Current weather of " + cityName + " (" + countryName + ")"
                                 + "\n Temp: " + df.format(temp) + " °C"
                                 + "\n Description: " + description;
                         textResult.setText(output);
+                        imageView.setImageResource(R.drawable.shower);
 
-                        Resources res = getResources();
+                      /*  Resources res = getResources();
                         int resID = res.getIdentifier(description.split(" ")[0], "drawable", getPackageName());
                         Drawable drawable = res.getDrawable(resID );
                         imageView.setImageDrawable(drawable );
                        // int hola= getResources().getIdentifier(description,"drawable",getPackageName());
-
+                        */
                         //imageView.setImageResource(hola);
                     } catch (JSONException e) {
                         e.printStackTrace();
